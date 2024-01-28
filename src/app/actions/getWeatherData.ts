fb75bea9841de44a80213b2b21552fb949470828
Markus <markuslokk@gmail.com>
2024-01-28 13:18:13 +0200

    change error message in  getWeatherData function, add api route for getting weather data

--- src/app/actions/getWeatherData.ts
+++ src/app/actions/getWeatherData.ts
@@ -1,7 +1,7 @@
 export const getWeatherData = async ({ lat, lon }: { lat: string; lon: string }) => {
 	const data = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/weather?lat=${lat}&lon=${lon}&appid=${process.env.OPEN_WEATHER_API_KEY}`);
 	if (!data.ok) {
-		throw new Error("Failed to fetch data");
+		throw new Error("Failed to fetch data weather data");
 	}
 
 	return data.json();
--- src/app/api/weather/route.ts
+++ src/app/api/weather/route.ts
@@ -1,7 +1,23 @@
-export async function GET() {
-	const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=59.43696079999999&lon=24.7535746&units=metric&appid=${process.env.OPEN_WEATHER_API_KEY}`);
+export async function GET(request: Request) {
+	const { searchParams } = new URL(request.url);
+	const lat = searchParams.get("lat");
+	const lon = searchParams.get("lon");
+	const appid = searchParams.get("appid");
+
+	if (!appid) {
+		return Response.json({ message: "API key not found in environment variables" }, { status: 401 });
+	}
+
+	if (!lat || !lon) {
+		return Response.json({ message: "Missing params" }, { status: 400 });
+	}
+
+	const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${appid}`, {
+		next: { revalidate: 900 },
+	});
+
 	if (!res.ok) {
-		throw new Error("Failed to fetch");
+		throw new Error("Failed to fetch weather data");
 	}
 
 	const data = await res.json();
