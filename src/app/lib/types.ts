ee6bb97b0d6008d18929946a16be5da1ea9225d9
Markus <markuslokk@gmail.com>
2024-01-30 00:51:51 +0200

    add types for forecast data

--- src/app/lib/types.ts
+++ src/app/lib/types.ts
@@ -65,3 +65,44 @@ export type City = {
 	sunrise: number;
 	sunset: number;
 };
+
+type Temperature = {
+	day: number;
+	min: number;
+	max: number;
+	night: number;
+	eve: number;
+	morn: number;
+};
+
+type FeelsLike = {
+	day: number;
+	night: number;
+	eve: number;
+	morn: number;
+};
+
+export type ForecastData = {
+	dt: number;
+	sunrise: number;
+	sunset: number;
+	temp: Temperature;
+	feels_like: FeelsLike;
+	pressure: number;
+	humidity: number;
+	weather: Weather[];
+	speed: number;
+	deg: number;
+	gust: number;
+	clouds: number;
+	pop: number;
+	rain?: number;
+};
+
+export type TenDayForecastData = {
+	city: City;
+	cod: string;
+	message: number;
+	cnt: number;
+	list: ForecastData[];
+};
