8f9e150a86c81f7ec667f448ae038777ed688961
Markus <markuslokk@gmail.com>
2024-02-05 00:48:42 +0200

    add logic for converting city name to coordinates

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -1,6 +1,25 @@
+"use client";
+
+import { getGeoLocation } from "@/actions/getGeoLocation";
+import { useRouter } from "next/navigation";
+import { FormEvent } from "react";
+
 export default function SearchBar() {
+	const router = useRouter();
+
+	const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		const formData = new FormData(event.currentTarget);
+		const city = formData.get("city") as string;
+		event.currentTarget.reset();
+		const res = await getGeoLocation({ city });
+
+		const newLat = res[0].lat;
+		const newLon = res[0].lon;
+		router.push(`/search?lat=${newLat}&lon=${newLon}`);
+	};
 	return (
-		<div>
+		<form onSubmit={handleSubmit}>
 			<label
 				htmlFor="search"
 				className="sr-only">
@@ -9,8 +28,7 @@ export default function SearchBar() {
 			<div className="relative mt-2 flex items-center">
 				<input
 					type="text"
-					name="search"
-					id="search"
+					name="city"
 					placeholder="Search for city..."
 					className="block w-full rounded-md border-0 py-2 pr-14 text-gray-200 shadow-sm ring-1 ring-inset ring-gray-600 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-500 bg-transparent sm:text-sm sm:leading-6"
 				/>
@@ -18,6 +36,6 @@ export default function SearchBar() {
 					<kbd className="inline-flex items-center rounded border border-gray-600 px-1.5 font-sans text-xs text-gray-400">⌘K</kbd>
 				</div>
 			</div>
-		</div>
+		</form>
 	);
 }
