9b820009527e6bdcd832cb39f14ad3ce07a2b9dd
Markus <markuslokk@gmail.com>
2024-02-05 00:46:26 +0200

    update actions to remove appId from request file

--- src/actions/getForecastData.ts
+++ src/actions/getForecastData.ts
@@ -1,5 +1,5 @@
 export const getForecastData = async ({ lat, lon }: { lat: string; lon: string }) => {
-	const data = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/weather/forecast?lat=${lat}&lon=${lon}&appid=${process.env.OPEN_WEATHER_API_KEY}`);
+	const data = await fetch(`${process.env.LOCAL_DEV_SERVER_URL}/api/weather/forecast?lat=${lat}&lon=${lon}`);
 	if (!data.ok) {
 		throw new Error("Failed to fetch data weather data");
 	}
--- src/app/api/weather/forecast/route.ts
+++ src/app/api/weather/forecast/route.ts
@@ -2,17 +2,12 @@ export async function GET(request: Request) {
 	const { searchParams } = new URL(request.url);
 	const lat = searchParams.get("lat");
 	const lon = searchParams.get("lon");
-	const appid = searchParams.get("appid");
-
-	if (!appid) {
-		return Response.json({ message: "API key not found in environment variables" }, { status: 401 });
-	}
 
 	if (!lat || !lon) {
 		return Response.json({ message: "Missing params" }, { status: 400 });
 	}
 
-	const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${appid}`, {
+	const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.OPEN_WEATHER_API_KEY}`, {
 		next: { revalidate: 900 },
 	});
 
