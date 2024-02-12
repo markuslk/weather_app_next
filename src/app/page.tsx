0ce02224507469eafdd4444e280ac8e4410bffaa
Markus <markuslokk@gmail.com>
2024-02-12 19:11:33 +0200

    create initial ForecastWeather component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,10 @@ import CurrentWeather from "./components/CurrentWeather";
 import { DEFAULT_LOCATAION } from "./lib/config";
 import { getWeatherData } from "@/actions/getWeatherData";
 import { notFound } from "next/navigation";
-import { OpenWeatherData, TenDayForecastData } from "./lib/types";
+import { ForecastDataResponse, OpenWeatherData } from "./lib/types";
 import SearchBar from "./components/SearchBar";
 import { getForecastData } from "@/actions/getForecastData";
+import ForecastWeather from "./components/ForecastWeather";
 
 export const metadata: Metadata = {
 	title: `${DEFAULT_LOCATAION.city} - Weather Forecast`,
@@ -17,21 +18,24 @@ export default async function Home() {
 
 	const CurrentWeatherRequest: OpenWeatherData = await getWeatherData({ lat, lon });
 
-	const WeatherForecastRequest: TenDayForecastData = await getForecastData({ lat, lon });
+	const WeatherForecastRequest: ForecastDataResponse = await getForecastData({ lat, lon });
 
 	const [current_weather_data, forecast_weather_data] = await Promise.all([CurrentWeatherRequest, WeatherForecastRequest]);
 
 	if (!current_weather_data || !forecast_weather_data) return notFound();
 
+	console.log(forecast_weather_data);
+
 	return (
 		<>
 			<div className="p-8 md:p-32 flex justify-center flex-col items-center gap-12">
 				<SearchBar />
-				<div className="flex flex-col gap-4 md:flex-row w-full justify-center">
+				<div className="flex gap-4 flex-col w-full items-center">
 					<CurrentWeather
 						data={current_weather_data}
 						city={forecast_weather_data.city}
 					/>
+					<ForecastWeather data={forecast_weather_data.list} />
 				</div>
 			</div>
 		</>
