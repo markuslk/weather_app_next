f7ce7f323e54383eace441ea1e09916ff50fe695
Markus <markuslokk@gmail.com>
2024-02-12 19:11:13 +0200

    configure route for forecast weather info

--- src/app/api/weather/forecast/route.ts
+++ src/app/api/weather/forecast/route.ts
@@ -2,12 +2,13 @@ export async function GET(request: Request) {
 	const { searchParams } = new URL(request.url);
 	const lat = searchParams.get("lat");
 	const lon = searchParams.get("lon");
+	const TIMESTAMPS = 10;
 
 	if (!lat || !lon) {
 		return Response.json({ message: "Missing params" }, { status: 400 });
 	}
 
-	const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.OPEN_WEATHER_API_KEY}`, {
+	const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&cnt=${TIMESTAMPS}&appid=${process.env.OPEN_WEATHER_API_KEY}`, {
 		next: { revalidate: 900 },
 	});
 
